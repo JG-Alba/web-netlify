0388daac592297ceb4af8a28ca5772a947010f42
JG-Alba <jg.alba97@gmail.com>
2024-05-10 11:46:41 +0200

    cambios de nieve

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ function parallax(e) {
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const numFlakes = 100; // Cantidad de copos de nieve
+
+    for (let i = 0; i < numFlakes; i++) {
+        createFlake();
+    }
+
+    function createFlake() {
+        const flake = document.createElement("div");
+        flake.className = "snowflake";
+        flake.style.left = Math.random() * 100 + "vw"; // Posición aleatoria en el ancho de la ventana
+        flake.style.animationDuration = (Math.random() * 5 + 5) + "s"; // Duración de la animación aleatoria entre 5 y 10 segundos
+        flake.style.animationDelay = -Math.random() * 10 + "s"; // Retardo de la animación aleatorio entre -10 y 0 segundos
+        document.querySelector(".snowflakes").appendChild(flake);
+    }
+});
